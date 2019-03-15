d506dc4ade1876b6c3d177e404f3552ba4c6ffa4
Hidenori Miyoshi <miyohide@gmail.com>
2019-03-15 21:11:35 +0900

    19. $nextTick.

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 const vm = new Vue({
     el: "#app",
-    data: {a: 0},
+    data: {v: "クリックして編集", f: false},
+    methods: {
+        c: function () {
+            this.f = true;
+            // Vue.nextTick https://jp.vuejs.org/v2/api/#Vue-nextTick
+            this.$nextTick(function () { this.$refs.r1.focus(); });
+        },
+    },
 });
