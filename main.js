d7f9e862b69a47f5af4774e9ed323ef1093ef540
Hidenori Miyoshi <miyohide@gmail.com>
2019-03-18 20:27:16 +0900

    23. expand row line.

--- main.js
+++ main.js
@@ -1,15 +1,4 @@
 const vm = new Vue({
     el: "#app",
-    data: {good: 0, bad: 0},
-    // created https://jp.vuejs.org/v2/api/#created
-    created: function () {
-        // vm.$once https://jp.vuejs.org/v2/api/#vm-once
-        this.$once("foo", function(e) { this[e.target.dataset.key] += 1; });
-    },
-    methods: {
-        a: function (e) {
-            // vm.$emit https://jp.vuejs.org/v2/api/#vm-emit
-            this.$emit("foo", e);
-        },
-    },
+    data: {a: ""},
 });
