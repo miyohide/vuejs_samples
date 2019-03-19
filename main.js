a0b1600b8eaff2a45e6d333c2bcdfb3f4d2361d9
Hidenori Miyoshi <miyohide@gmail.com>
2019-03-19 21:49:41 +0900

    25. event.

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 const vm = new Vue({
     el: "#app",
-    data: {a: ""},
+    data: {
+        a: Math.floor(Math.random() * 10),
+        b: Math.floor(Math.random() * 10),
+    },
+    methods: {
+        c: function (e) {
+            if (parseInt(window.prompt(`${this.a} + ${this.b}は？`)) != (this.a + this.b)) {
+                e.preventDefault();
+                alert("不正解");
+            } else {
+                e.preventDefault();
+            }
+        },
+    },
 });
