802f33aae8047951fba7d2a9391fc8812bf45a2b
Hidenori Miyoshi <miyohide@gmail.com>
2019-03-20 22:26:37 +0900

    26. multi question one page.

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
 const vm = new Vue({
     el: "#app",
     data: {
-        a: Math.floor(Math.random() * 10),
-        b: Math.floor(Math.random() * 10),
+        list: ["接客", "清潔感", "早さ", "雰囲気", "味", "値段", "空調"],
+        index: 0,
+        a: null,
+        show: false,
+        answers: [],
     },
     methods: {
-        c: function (e) {
-            if (parseInt(window.prompt(`${this.a} + ${this.b}は？`)) != (this.a + this.b)) {
-                e.preventDefault();
-                alert("不正解");
-            } else {
-                e.preventDefault();
-            }
+        next: function (e) {
+            this.answers.push(this.a);
+            // _.delay https://lodash.com/docs/4.17.11#delay
+            // 500ミリ秒待って処理を実行する
+            _.delay(() => { this.index++; this.a = null;}, 500);
+        },
+    },
+    // computed https://jp.vuejs.org/v2/api/#computed
+    computed: {
+        q: function () {
+            // 現在のインデックスから配列の長さで割ったものを返す。
+            // このため、どんなにインデックスの値が大きくても
+            // 配列の長さを超えることはない（無限ループになるが）。
+            return this.list[this.index % this.list.length];
         },
     },
 });
