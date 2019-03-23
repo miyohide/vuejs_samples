11e2551e536fac350a0059f6761401c36bb95062
Hidenori Miyoshi <miyohide@gmail.com>
2019-03-23 18:42:25 +0900

    30. generate password.

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ const vm = new Vue({
     el: "#app",
     data: {
         a: "",
-        b: "",
     },
-    watch: {
-        a: function (v) {
-            this.a = v.replace(/[A-Za-z0-9]/g, function(s) { return String.fromCharCode(s.charCodeAt(0) + 65248)});
-        },
-        b: function(v) {
-            this.b = v.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(s) { return String.fromCharCode(s.charCodeAt(0) - 65248) })
-        },
-    }
+    methods: {
+        g: function () {
+            // Math.rand()は0以上1未満の数値を返す。これを大きな数値を掛けて
+            // Math.floor()で整数化。36進数に変換してランダムな文字列を返す。
+            // ある程度の文字列の長さが欲しいので、Math.random()にかける数を
+            // 大きくして大きな数値を得るようにする。
+            this.a = Math.floor(Math.random() * 100000000000000000).toString(36)
+        }
+    },
 });
