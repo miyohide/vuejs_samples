f74fb47cf4fe024438b440208c9be27d30b0e0c0
Hidenori Miyoshi <miyohide@gmail.com>
2019-03-25 20:23:18 +0900

    31. password strength.

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 const vm = new Vue({
     el: "#app",
     data: {
-        a: "",
+        p: "",
     },
-    methods: {
-        g: function () {
-            // Math.rand()は0以上1未満の数値を返す。これを大きな数値を掛けて
-            // Math.floor()で整数化。36進数に変換してランダムな文字列を返す。
-            // ある程度の文字列の長さが欲しいので、Math.random()にかける数を
-            // 大きくして大きな数値を得るようにする。
-            this.a = Math.floor(Math.random() * 100000000000000000).toString(36)
-        }
+    computed: {
+        s: function () {
+            let s = 0;
+            s += /.{8,}/.test(this.p) ? 20 : 0;
+            s += /\d/.test(this.p) ? 20 : 0;
+            s += /[a-z]/.test(this.p) ? 20 : 0;
+            s += /[A-Z]/.test(this.p) ? 20 : 0;
+            s += /[#!&]/.test(this.p) ? 20 : 0;
+            return s;
+        },
     },
 });
