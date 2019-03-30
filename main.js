c2e4b3d8b0789050531ab0e390ed82184cd9f1b7
Hidenori Miyoshi <miyohide@gmail.com>
2019-03-31 08:27:02 +0900

    35. move focus when input full length.

--- main.js
+++ main.js
@@ -1,21 +1,12 @@
 const vm = new Vue({
     el: "#app",
     data: {
-        v: "",
-        s: [],
+        a: "",
+        b: "",
+        c: "",
     },
-    methods: {
-        run: function () {
-            if (this.v != "") {
-                if (/\d+/.test(this.v)) {
-                    this.s.push(this.v);
-                } else {
-                    const l = this.s.pop();
-                    const r = this.s.pop();
-                    this.s.push(eval(`${l}${this.v}${r}`));
-                }
-                this.v = "";
-            }
-        },
+    watch: {
+        a: function(v) { if (v.length >= 3) { this.$refs.r2.focus(); } },
+        b: function(v) { if (v.length >= 4) { this.$refs.r3.focus(); } },
     },
 });
