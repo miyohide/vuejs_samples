25aae43373a3e5136b686b19a098996203328b4c
Hidenori Miyoshi <miyohide@gmail.com>
2019-04-01 20:34:04 +0900

    36. when push delete, back previous field.

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ const vm = new Vue({
         b: "",
         c: "",
     },
+    methods: {
+        b2: function (e) {if (e.target.value == "") { this.$refs.r1.focus() } },
+        b3: function (e) {if (e.target.value == "") { this.$refs.r2.focus() } },
+    },
     watch: {
         a: function(v) { if (v.length >= 3) { this.$refs.r2.focus(); } },
         b: function(v) { if (v.length >= 4) { this.$refs.r3.focus(); } },
